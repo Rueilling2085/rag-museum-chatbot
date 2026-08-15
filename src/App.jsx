b47d86a8d3cfbe617227030aaf0604363b965e70
Rueilling2085 <your-email@example.com>
2026-08-15 22:23:39 +0800

    feat: add iOS status bar with Dynamic Island, time, and battery icons

--- src/App.jsx
+++ src/App.jsx
@@ -630,6 +630,30 @@ function App() {
     <>
       <div className="app-root">
         
+        {/* iOS Status Bar */}
+        <div className="ios-status-bar">
+          <div className="ios-time">9:41</div>
+          <div className="ios-dynamic-island">
+            <div className="ios-camera"></div>
+          </div>
+          <div className="ios-icons">
+            {/* Signal Icon */}
+            <svg className="ios-signal" viewBox="0 0 18 14" fill="currentColor">
+              <path d="M1 10.5h3V14H1v-3.5zm4.5-3.5h3V14h-3V7zm4.5-3.5h3V14h-3V3.5zm4.5-3.5h3V14h-3V0z"/>
+            </svg>
+            {/* WiFi Icon */}
+            <svg className="ios-wifi" viewBox="0 0 18 14" fill="currentColor">
+              <path d="M9 14c-1.8 0-3.4-.6-4.7-1.6l1.2-1.5c1 .8 2.2 1.1 3.5 1.1s2.5-.3 3.5-1.1l1.2 1.5C12.4 13.4 10.8 14 9 14zm0-4.5c-2.8 0-5.3-1-7.2-2.7L3 5.3c1.6 1.4 3.7 2.2 6 2.2s4.4-.8 6-2.2l1.2 1.5C14.3 8.5 11.8 9.5 9 9.5zM9 5C5.5 5 2.4 3.7.1 1.5L1.5 0C3.5 1.9 6.1 3 9 3s5.5-1.1 7.5-3l1.4 1.5C15.6 3.7 12.5 5 9 5z"/>
+            </svg>
+            {/* Battery Icon */}
+            <div className="ios-battery">
+              <div className="ios-battery-body">
+                <div className="ios-battery-level"></div>
+              </div>
+              <div className="ios-battery-tip"></div>
+            </div>
+          </div>
+        </div>
               {viewMode === 'landing' ? (
                 /* 展覽首頁視圖 */
                 <>
